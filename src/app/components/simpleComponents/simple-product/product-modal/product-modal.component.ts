c122a3f235e71b235f61ba73e9c9c809f8ccf646
Matheus Dias <matheussd7@gmail.com>
2025-05-20 12:05:43 -0300

    Adicionado modal de produto

---
+++ src/app/components/simpleComponents/simple-product/product-modal/product-modal.component.ts
@@ -0,0 +1,60 @@
+// product-modal.component.ts
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-modal',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './product-modal.component.html',
+  styleUrls: ['./product-modal.component.scss']
+})
+export class ProductModalComponent {
+  @Input() imageUrl: string = '';
+  @Input() productName: string = '';
+  @Input() description: string = '';
+  @Input() price: number = 0;
+
+  @Output() orderSubmitted = new EventEmitter<{
+    quantity: number;
+    observations: string;
+  }>();
+
+  isOpen: boolean = false;
+  quantity: number = 1;
+  observations: string = '';
+
+  open() {
+    this.isOpen = true;
+  }
+
+  close() {
+    this.isOpen = false;
+    this.resetForm();
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  submitOrder() {
+    this.orderSubmitted.emit({
+      quantity: this.quantity,
+      observations: this.observations
+    });
+    this.close();
+  }
+
+  private resetForm() {
+    this.quantity = 1;
+    this.observations = '';
+  }
+
+}
--- src/app/components/simpleComponents/simple-product/simple-product.component.ts
+++ src/app/components/simpleComponents/simple-product/simple-product.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { SanitizePipe } from '../../../pipes/saniteize.pipe';
-
+import { ProductModalComponent } from './product-modal/product-modal.component';
 
 @Component({
   selector: 'app-simple-product',
   standalone: true,
-  imports: [CommonModule, SanitizePipe],
+  imports: [CommonModule, SanitizePipe, ProductModalComponent],
   templateUrl: './simple-product.component.html',
-  styleUrl: './simple-product.component.scss'
+  styleUrl: './simple-product.component.scss',
 })
 export class SimpleProductComponent {
   @Input() format: '1x1' | '1x2' | '2x1' | '2x2' = '1x1';
@@ -16,6 +16,14 @@ export class SimpleProductComponent {
   @Input() imageUrl: string = '';
   @Input() productName: string = '';
   @Input() description: string = '';
-  @Input() price: string = '';
+  @Input() price: number = 0;
 
+  @ViewChild(ProductModalComponent) productModal!: ProductModalComponent;
+  openModal() {
+    this.productModal.open();
+  }
+
+  handleOrder(order: { quantity: number; observations: string }) {
+    console.log('Pedido enviado:', order);
+  }
 }
