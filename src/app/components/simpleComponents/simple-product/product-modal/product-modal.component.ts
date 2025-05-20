5fc5e3e2c8717ec86e12f3afbb1b3f2b33885cb6
Matheus Dias <matheussd7@gmail.com>
2025-05-20 15:46:08 -0300

    modal de produto em fullscreen para telas mobile

--- src/app/components/simpleComponents/simple-product/product-modal/product-modal.component.ts
+++ src/app/components/simpleComponents/simple-product/product-modal/product-modal.component.ts
@@ -1,4 +1,3 @@
-// product-modal.component.ts
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
