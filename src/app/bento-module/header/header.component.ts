4e6721be79f15fdfc563f179c870395d86e9a7d6
Matheus Dias <matheussd7@gmail.com>
2025-08-07 15:48:10 -0300

    Barra de pesquisa

--- src/app/bento-module/header/header.component.ts
+++ src/app/bento-module/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CartService } from '../../services/cart-service/cart.service';
 import { CartComponent } from '../cart/cart.component';
 
@@ -7,14 +7,20 @@ import { CartComponent } from '../cart/cart.component';
   standalone: true,
   imports: [CartComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   isCartOpen = false;
+
+  @Output() search = new EventEmitter<string>();
 
   constructor(public _cartService: CartService) {}
 
   toggleCart() {
     this.isCartOpen = !this.isCartOpen;
   }
+
+  onSearchInput(value: string) {
+    this.search.emit(value);
+}
 }
