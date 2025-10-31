d2ed178ad0b2ebbe5ed58e951639f8306fa9ccca
Matheus Dias <matheussd7@gmail.com>
2025-10-31 10:56:43 -0300

    cleanup

--- src/app/pipes/sanitize.pipe.ts
+++ src/app/pipes/sanitize.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import sanitizeHtml from 'sanitize-html';
+import DOMPurify from 'dompurify';
 
 @Pipe({
   name: 'sanitize',
@@ -10,17 +10,68 @@ export class SanitizePipe implements PipeTransform {
   constructor(private domSanitizer: DomSanitizer) {}
 
   transform(value: string): SafeHtml {
-    // Sanitiza o HTML removendo scripts, eventos e tags perigosas
-    const clean = sanitizeHtml(value, {
-      allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'h1', 'h2']),
-      allowedAttributes: {
-        ...sanitizeHtml.defaults.allowedAttributes,
-        img: ['src', 'alt', 'title', 'width', 'height', 'style'],
-        a: ['href', 'name', 'target', 'rel'],
-      },
-      allowedSchemes: ['http', 'https', 'data'],
-      allowedSchemesByTag: {},
-      allowedIframeHostnames: [],
+    // Sanitiza o HTML usando DOMPurify (browser-safe)
+    const clean = DOMPurify.sanitize(value, {
+      ALLOWED_TAGS: [
+        'img',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'p',
+        'a',
+        'b',
+        'i',
+        'u',
+        'strong',
+        'em',
+        'ul',
+        'ol',
+        'li',
+        'br',
+        'span',
+        'div',
+        'table',
+        'thead',
+        'tbody',
+        'tr',
+        'th',
+        'td',
+        'hr',
+        'blockquote',
+        'pre',
+        'code',
+        'sup',
+        'sub',
+        'small',
+        'mark',
+        'del',
+        'ins',
+        'figure',
+        'figcaption',
+      ],
+      ALLOWED_ATTR: [
+        'src',
+        'alt',
+        'title',
+        'width',
+        'height',
+        'style',
+        'href',
+        'name',
+        'target',
+        'rel',
+        'class',
+        'id',
+        'align',
+        'valign',
+        'colspan',
+        'rowspan',
+        'data-*',
+      ],
+      ALLOWED_URI_REGEXP: /^(?:(?:https?|data):|[^a-z]|[a-z+.-]+(?:[^a-z+.-]|$))/i,
     });
     return this.domSanitizer.bypassSecurityTrustHtml(clean);
   }
